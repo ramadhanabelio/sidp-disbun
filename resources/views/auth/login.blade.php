d1286cfc030249bc6c3bdfd0409794541b84c63f
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2024-10-31 03:21:03 +0700

    style: update button color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,7 +72,7 @@
                                         </div>
 
                                         <div class="col-12">
-                                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                                            <button class="btn btn-success w-100" type="submit">Login</button>
                                         </div>
                                     </form>
                                 </div>
