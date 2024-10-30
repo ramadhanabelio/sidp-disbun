af1264bdbd91992e307f84679a6184af2b644572
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2024-10-31 01:28:16 +0700

    fix: fix auth login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function showLoginForm()
+    public function index()
     {
         return view('auth.login');
     }
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::get('/', [LoginController::class, 'index'])->name('login');
+Route::post('/', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
